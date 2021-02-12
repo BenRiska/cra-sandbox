74cf1a69e980331423755900c39166d030495a16
bentaylor <ben10taylor10@hotmail.com>
2021-02-12 10:31:32 +0000

    cra env

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import "./styles/style.css"
+
+
+const App = () => {
+  return <div>Hello World...</div>
+}
+
+reactDOM.render(<App />, document.getElementById('root'))
